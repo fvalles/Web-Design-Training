6ff339d14ca4054170170d16a87e80c26d79859d
Federico Valles <fedeva_37@hotmail.com>
2020-01-23 23:17:10 -0300

    Ninth and Tenth tests both working

--- js finger exercises/react/ecmascript/01 - spread/index.js
+++ js finger exercises/react/ecmascript/01 - spread/index.js
@@ -30,3 +30,28 @@ export const reverseMerge = (firstArray, secondArray) => {
   let revMergedArray = [...secondArray, ...firstArray];
   return revMergedArray;
 }
+
+export const filterAttribs = (obj) => {
+  let objKeys = Object.keys(obj);
+  let objValues = Object.values(obj);
+  let newObjValues = [];
+
+  let newObjKeys = objKeys.filter((key, index) => {
+    if (key !== 'a' && key !== 'b') {
+      newObjValues.push(objValues[index]);
+      return key;
+    }
+  });
+
+  const assignKeyValue = (obj, key, val) => {
+    obj[key] = val;
+  }
+
+  let filteredObj = {};
+
+  newObjKeys.forEach((key, index) => {
+    assignKeyValue(filteredObj, key, newObjValues[index])
+  });
+
+  return filteredObj;
+}
