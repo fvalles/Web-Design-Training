e00b7e46501cf77bc514dea8e51ad76b933ba9f8
Federico Valles <fedeva_37@hotmail.com>
2020-01-25 14:05:29 -0300

    First test -delay works well- working OK

--- js finger exercises/react/ecmascript/02 - promises/index.js
+++ js finger exercises/react/ecmascript/02 - promises/index.js
@@ -1,7 +1,13 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(delayedTime) {
+    const initialTime = Date.now();
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const finalTime = Date.now();
+            resolve (finalTime - initialTime);
+        }, delayedTime);
+    });
 }
 
 export function asyncDelay() {
