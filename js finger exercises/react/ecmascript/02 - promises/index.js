2054e6e4ad169284b7e5a6b845334548917c48de
Federico Valles <fedeva_37@hotmail.com>
2020-01-27 16:20:00 -0300

    Style modifications made

--- js finger exercises/react/ecmascript/02 - promises/index.js
+++ js finger exercises/react/ecmascript/02 - promises/index.js
@@ -1,21 +1,21 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
 export function delay(delayTime) {
-    const initialTime = Date.now();
-    return new Promise((resolve, reject) => {
-        if (delayTime < 5000) {
-            setTimeout(() => {
-                const finalTime = Date.now();
-                resolve (finalTime - initialTime);
-            }, delayTime);
-        } else {
-            const error = new Error('This time is too much !');
-            reject(error);
-        }
-    });
+  const initialTime = Date.now();
+  return new Promise((resolve, reject) => {
+    if (delayTime < 5000) {
+      setTimeout(() => {
+        const finalTime = Date.now();
+        resolve(finalTime - initialTime);
+      }, delayTime);
+    } else {
+      const error = new Error('This time is too much !');
+      reject(error);
+    }
+  });
 }
 
 export async function asyncDelay(delayTime) {
-    const delayedTime = await delay(delayTime);
-    return delayedTime;
+  const delayedTime = await delay(delayTime);
+  return delayedTime;
 }
